9c5b29cbf3e1dee946ab5bcf1a4d9ecca3c30e80
Rakesh <iamrakeshkumar@pm.me>
2022-03-05 13:09:56 +0530

    added:getIndexToIns

---
+++ src/day1/getIndexToIns/getIndexToIns.js
@@ -0,0 +1,25 @@
+/** (*)
+ * Return the lowest index at which a value (second argument) should
+ * be inserted into an array (first argument) once it has been sorted.
+ * The returned value should be a number.
+ * For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because
+ * it is greater than 1 (index 0), but less than 2 (index 1).
+ * Likewise, getIndexToIns([20,3,5], 19) should return 2 because
+ * once the array has been sorted it will look like [3,5,20] and
+ * 19 is less than 20 (index 2) and greater than 5 (index 1).
+ */
+
+function getIndexToIns(array, valueToInsert) {
+  const sortedArray = array.sort((element1, element2) => element1 - element2);
+  let expetedIndex = sortedArray.length;
+  sortedArray.some((element, index) => {
+    if (element >= valueToInsert) {
+      expetedIndex = index;
+      return true;
+    }
+  });
+
+  return expetedIndex;
+}
+
+module.exports = getIndexToIns;

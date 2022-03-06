a8615b8575f8eb9827d8856ae44e14200e53628e
Rakesh <iamrakeshkumar@pm.me>
2022-03-06 13:22:54 +0530

    added:alternateCharcter

---
+++ src/day2/22-alternateCharacter/alternateCharcter.js
@@ -0,0 +1,21 @@
+function getNoOfDeletion(string) {
+  let timesToDelete = 0;
+  for (let i = 1; i < string.length; i += 1) {
+    if (string.charAt(i) === string.charAt(i - 1)) {
+      string.slice(i, i + 1);
+      timesToDelete += 1;
+    }
+  }
+  return timesToDelete;
+}
+
+function alternatingCharacters(arrayStrings) {
+  const timesToDeleteArray = [];
+  arrayStrings.forEach((string) => {
+    const timesToDelete = getNoOfDeletion(string);
+    timesToDeleteArray.push(timesToDelete);
+  });
+  return timesToDeleteArray;
+}
+
+module.exports = alternatingCharacters;

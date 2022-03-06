b1e8fae1d789fa8d0d43b3b8f3907d650e4679e7
Rakesh <iamrakeshkumar@pm.me>
2022-03-06 12:46:01 +0530

    added:squareNumbersArray

---
+++ src/day2/16-squareNumberArray/squareNumberArray.js
@@ -0,0 +1,18 @@
+class ArgumentError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ArgumentError";
+  }
+}
+
+function squareNumbersArray(...args) {
+  const isContainsNonNumericalArgs = args.filter((arg) =>
+    Number.isNaN(parseInt(arg, 10))
+  );
+  if (isContainsNonNumericalArgs.length) {
+    throw new ArgumentError("My custom error");
+  }
+  return args.map((num) => num ** 2);
+}
+
+module.exports = squareNumbersArray;
---
+++ src/day2/16-squareNumberArray/squareNumberArray.test.js
@@ -0,0 +1,13 @@
+const squareNumbersArray = require("./squareNumberArray");
+
+describe("squareNumbersArray", () => {
+  test("should throw an error if array of numbers is not passed as argument", () => {
+    const myArray = [1, 2, "string", 4];
+    expect(() => squareNumbersArray(...myArray)).toThrow("My custom error");
+  });
+  test("should return square array", () => {
+    const myArray = [1, 3, 2, 6];
+    const result = [1, 9, 4, 36];
+    expect(squareNumbersArray(...myArray)).toEqual(result);
+  });
+});

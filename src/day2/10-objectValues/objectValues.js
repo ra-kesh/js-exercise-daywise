550c830f0f7f812290c74b4ffcf7cc491858856d
Rakesh <iamrakeshkumar@pm.me>
2022-03-06 12:03:41 +0530

    added:objectValues

---
+++ src/day2/10-objectValues/objectValues.js
@@ -0,0 +1,10 @@
+// (*)
+// const values = (obj) => {
+//   // Return all of the values of the object's own properties.
+//   // Ignore functions
+//   // http://underscorejs.org/#values
+// };
+
+const objectValues = (obj) => Object.values(obj);
+
+module.exports = objectValues;
